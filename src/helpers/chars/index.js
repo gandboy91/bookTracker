eaaf83751f35d9379a8f971cbe7f4daa3fb779af
ivan <ivan_sibir@mail.ru>
2019-02-20 23:55:08 +0700

    added styles

--- src/helpers/chars/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * chars constants
- */
-export const NBSP = String.fromCharCode(160);
-export const RIGHT_ARROW = String.fromCharCode(8594);
-export const RETURN_ARROW = String.fromCharCode(8629);
