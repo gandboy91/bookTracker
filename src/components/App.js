0d479b75ddf5482c62728b071d9cfa803fa94931
ivan <ivan_sibir@mail.ru>
2019-02-20 23:46:38 +0700

    added root component

--- src/components/App.js
+++ src/components/App.js
@@ -1,80 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import queryString from "query-string";
-import {fetchBookList} from "./requests/index";
-import './App.css';
+import BookTracker from "./BookTracker";
+import './styles/App.css';
 
-const TABS = [
-    {
-        tab: 'toread',
-        title: 'To read'
-    },
-    {
-        tab: 'inprogress',
-        title: 'In progress'
-    },
-    {
-        tab: 'done',
-        title: 'Done'
-    },
-];
-
-const Tab = ({tab, title, active}) =>
-    <Link to={`/?tab=${tab}`} className={`tab ${active ? 'active' : ''}`}>
-        {
-            active
-                ? <strong>{title}</strong>
-                : title
-        }
-    </Link>;
-
-class BookTracker extends Component {
-    state = {
-        ...this.props.stateFromSession,
-        books: []
-    }
-
-    componentDidMount() {
-        fetchBookList('smallList.json')
-            .then(({items}) => this.setState({books: items}))
-            .catch(console.warn)
-    }
-
-    render() {
-        const {location: {search}} = this.props;
-        const {tab} = queryString.parse(search);
-        const currentTab = tab || TABS[0].tab;
-        return <div className="tracker">
-            <div className="tabs">
-                {
-                    TABS.map(({tab, title}, key) => <Tab
-                            key={key}
-                            tab={tab}
-                            title={title}
-                            active={tab === currentTab}
-                        />
-                    )
-                }
-            </div>
-        </div>
-    }
-}
-
+/**
+ * 404 component for wrong query
+ */
 const WrongRoute = props => <h1>Wrong url, dude!</h1>
 
+/**
+ * root Component
+ * perfoms basic routing logic
+ */
 class App extends Component {
     static propTypes = {
-        stateFromSession: PropTypes.object
+        stateFromStorage: PropTypes.object
     }
 
     static defaultProps = {
-        stateFromSession: {}
+        stateFromStorage: {}
     }
 
     render() {
-        const stateFromSession = this.props.stateFromSession || {};
-        const bookTrackerRender = props => <BookTracker {...props} stateFromSession={stateFromSession} />;
+        const {stateFromStorage} = this.props;
+        const bookTrackerRender = props => <BookTracker {...props} stateFromStorage={stateFromStorage} />;
 
         return <div>
             <Router>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {STORAGE_KEY} from "./constants/index";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+const stateFromStorage = localStorage ? localStorage.getItem(STORAGE_KEY) : null;
+console.log(JSON.parse(stateFromStorage));
+ReactDOM.render(
+    <App stateFromStorage={stateFromStorage ? JSON.parse(stateFromStorage) : {}}/>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note this comes with some pitfalls.1
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
